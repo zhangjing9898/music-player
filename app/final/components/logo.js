446e6cbd616b6a63f82e8a55777b737b5e954d8f
zhangjing9898 <1105249307@qq.com>
2018-01-30 10:12:18 +0800

    react-music-player
    基于react开发的一款音乐播放器

---
+++ app/final/components/logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+require('./logo.less');
+
+let Logo = React.createClass({
+    render() {
+        return (
+        	<div className="row components-logo">
+                <a href="/">
+                    <img src="/static/images/logo.png" width="40" alt="" className="-col-auto"/>
+                    <h1 className="caption">Music Player Build By React</h1>
+                </a>
+        	</div>
+        );
+    }
+});
+
+export default Logo;
